ea4a664b40ee53510d805afd1737c92a372fa5e4
Muhammad Rendra <rendra@garuda.io>
2020-12-15 19:09:51 +0700

    work v0.0.3

--- src/utils/useNotification.js
+++ src/utils/useNotification.js
@@ -1,4 +1,5 @@
 const useNotification = ({ soundURL, onClickURL, onShow, onClick }) => {
+  /* encapsulated function */
   const showNotification = (msg = '') => {
     const notification = new Notification(msg)
     // on show
@@ -8,44 +9,33 @@ const useNotification = ({ soundURL, onClickURL, onShow, onClick }) => {
       if (typeof soundURL === 'string' && soundURL.slice(soundURL.length - 4, soundURL.length) === '.mp3') {
         const audio = new Audio(soundURL)
         const playedPromise = audio.play()
-        if (playedPromise) {
-          playedPromise.catch((e) => {
-            console.log(e)
-            if (e.name === 'NotAllowedError' || e.name === 'NotSupportedError') {
-              console.log(e.name)
-            }
-          }).then((a, b, c) => {
-            console.log('playing sound !!!', { a, b, c })
-          })
-        }
+        playedPromise && playedPromise
+          .then(() => console.log('playing sound !!!'))
+          .catch((e) => (e.name === 'NotAllowedError' || e.name === 'NotSupportedError') && console.log(e.name))
       }
       // execute onShow props
-      if (typeof onShow === 'function') {
-        onShow(e)
-      }
+      if (typeof onShow === 'function') onShow(e)
     }
 
     // on click
     notification.onclick = (e) => {
       e.preventDefault()
       // execute onClick props
-      if (typeof onClick === 'function') {
-        onClick(e)
-      }
+      if (typeof onClick === 'function') onClick(e)
       // open _self window
-      if (typeof onClickURL === 'string' && (onClickURL.includes('http://') || onClickURL.includes('https://'))) {
-        window.open(onClickURL, '_self')
-      }
+      if (typeof onClickURL === 'string' && (onClickURL.includes('http://') || onClickURL.includes('https://'))) window.open(onClickURL, '_self')
     }
   }
 
+  /* returned function */
   const handleNotification = (message = '') => {
     if (!Notification || !('Notification' in window)) alert('This browser does not support desktop notification')
     else if (Notification.permission === 'granted') showNotification(message)
     else if (Notification.permission !== 'denied') {
-      Notification.requestPermission().then(permission => {
-        permission === 'granted' && showNotification(message)
-      })
+      Notification
+        .requestPermission()
+        .then(permission => permission === 'granted' && showNotification(message))
+        .catch(e => console.log(e))
     }
   }
 
